e19b810911ed57d25d79b5c5f2deb53a253298ed
Tigran Arabadjyan <arabadj@gmail.com>
2020-12-11 10:56:53 +0400

    fix centos-httpd hints

--- centos-httpd/_help.sh
+++ centos-httpd/_help.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker build . -f CentOS7.Dockerfile -t arabadj/centos-httpd:7
+docker build . -f CentOS7.Dockerfile -t arabadj/centos-httpd:7.8-latest
+docker build . -f CentOS8.Dockerfile -t arabadj/centos-httpd:8.2-latest
 
-docker build . -f CentOS8.Dockerfile -t arabadj/centos-httpd:8.2.2004-1.0.1
 
+docker run -d --name centos-httpd arabadj/centos-httpd:7.8-latest
+docker run -d --name centos-httpd arabadj/centos-httpd:8.2-latest
 
-docker run -d --name centos-httpd arabadj/centos-httpd:8
-docker run -d --name centos-httpd -p 80:80/tcp arabadj/centos-httpd:8
-docker run -d --name centos-httpd -p 127.0.0.1:80:80/tcp arabadj/centos-httpd:8
-docker run -d --name centos-httpd -p 192.168.73.31:80:80/tcp arabadj/centos-httpd:8
+docker run -d --name centos-httpd -p 80:80/tcp arabadj/centos-httpd:7.8-latest
+docker run -d --name centos-httpd -p 80:80/tcp arabadj/centos-httpd:8.2-latest
 
 docker exec -it centos-httpd /bin/bash
 
