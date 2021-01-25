31eafece82a9eff0b099449d17229a006617a362
Tigran Arabadjyan <arabadj@gmail.com>
2021-01-26 00:42:10 +0400

    update

--- centos-debug/_help.sh
+++ centos-debug/_help.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+
 docker build . -f CentOS7.Dockerfile -t arabadj/centos-debug:7-latest
-docker build . -f CentOS8.Dockerfile -t arabadj/centos-debug:8-latest
-
-
-
 docker run -it --rm --name centos-debug arabadj/centos-debug:7-latest /bin/bash
-docker run -it --rm --name centos-debug arabadj/centos-debug:8-latest /bin/bash
 
-docker run -it --rm --name centos-debug --privileged --net=host arabadj/centos-debug:8.2-latest /bin/bash
+
+docker build . -f CentOS8.Dockerfile -t arabadj/centos-debug:8-latest
+docker run -it --rm --name centos-debug arabadj/centos-debug:8-latest /bin/bash
+docker run -it --rm --name centos-debug --privileged --net=host arabadj/centos-debug:8-latest /bin/bash
 
 
