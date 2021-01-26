fe019a2b40a6a776dbba1967c5c0fe9af969fc63
Tigran Arabadjyan <arabadj@gmail.com>
2021-01-26 10:00:39 +0400

    fix

--- ubuntu-pptpd/_help.sh
+++ ubuntu-pptpd/_help.sh
@@ -9,8 +9,8 @@ docker build . -f build-red.Dockerfile -t arabadj/ubuntu-pptpd:red
 docker image tag arabadj/ubuntu-pptpd:blue arabadj/ubuntu-pptpd:20.04-VERSION
 
 
-# docker run -d --restart always --name=ubuntu-pptpd --privileged --net=host \
-#     -v ~/.local/opt/vpnp/etc/ppp/chap-secrets:/etc/ppp/chap-secrets:ro arabadj/ubuntu-pptpd:20
+docker run -d --restart always --name=ubuntu-pptpd --privileged --net=host \
+    -v ~/.local/opt/vpnp/etc/ppp/chap-secrets:/etc/ppp/chap-secrets:ro arabadj/ubuntu-pptpd:20.04-1.0.2
 
 docker exec -it vpnp_svc_1 /bin/bash
 
